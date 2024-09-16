a84a49a5975bd0e833f1df0ae80430af58db6a7b
shunlog <artiombalan331@gmail.com>
2024-09-16 14:32:08 +0300

    make or3 elegant

--- three_valued_logic.py
+++ three_valued_logic.py
@@ -1,4 +1,5 @@
-from typing import Iterable, Union
+from typing import Collection, Union
+import pytest
 
 '''
 In Kleene's three-valued logic there are possible values are True, False and Unknown.
@@ -11,32 +12,33 @@ Examples:
 '''
 
 
-def or3(ls: Iterable[Union[bool, None]]) -> Union[bool, None]:
-    unknown_flag = False
-    for v in ls:
-        if v: return True
-        if v is None: unknown_flag = True
-    if unknown_flag:
-        return None
-    return False        
+def or3(ls: Collection[Union[bool, None]]) -> Union[bool, None]:
+    if ls is iter(ls):
+        raise Exception("You gave me an iterator instead of a Collection!")
+    if any(ls): return True
+    if all(i == False for i in ls): return False
+    return None
 
 def test_or3() -> None:
-    # test that it works on generators
-    assert or3((i for i in (None, False, True))) == True
+    assert or3([None, False, True]) == True
     assert or3((False, False)) == False
-    assert or3((i for i in (None, False, None))) == None
+    assert or3([None, False, None]) == None
 
-    
-def and3(ls: Iterable[Union[bool, None]]) -> Union[bool, None]:
-    unknown_flag = False
-    for v in ls:
-        if v == False: return False
-        if v is None: unknown_flag = True
-    if unknown_flag:
-        return None
-    return True
+    # I would add this test, but mypy is gonna complain.
+    # And then again, this exception is just a hack to help people who aren't using mypy,
+    # not really a requirement for the function
+    # with pytest.raises(Exception):
+    #     or3(i for i in [None, False, None])
+
+
+def and3(ls: Collection[Union[bool, None]]) -> Union[bool, None]:
+    if ls is iter(ls):
+        raise Exception("You gave me an iterator instead of a Collection!")
+    if any(v == False for v in ls): return False
+    if all(i for i in ls): return True
+    return None
 
 def test_and3() -> None:
-    assert and3((i for i in (None, True, False))) == False
+    assert and3([None, True, False]) == False
     assert and3((True, True)) == True
-    assert and3((i for i in (None, True, None))) == None
+    assert and3([None, True, None]) == None
