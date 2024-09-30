c58b6ddcc811f24af85204a4d4c14177c4d4e9e0
shunlog <artiombalan331@gmail.com>
2024-09-30 23:39:21 +0300

    add male/female

--- src/expert_system/spongebob_rules.py
+++ src/expert_system/spongebob_rules.py
@@ -1,44 +1,49 @@
+# tip: only use OR alternatives when strictly necessary.
+# tip: make sure you provide as much info on the individual level as possible: gender, hair, color
 
 spongebob_rules = {
-    "Spongebob": ({"sponge", "has a square head", "has square pants", 'wears a suit'},),
-    "Stanley": ({"sponge", "has a square head", "has tall body", 'wears a suit'},),
+    "Spongebob": ({"sponge", "has a square head", "has square pants",
+                   'wears a suit', 'is male'},),
+    "Stanley": ({"sponge", "has a square head", "has tall body",
+                 'wears a suit', 'is male'},),
     "sponge": ({"is yellow", "has holes"},),
 
-    "Squidward": ({'octopus', 'wears a brown t-shirt'},),
-    "Squilliam": ({'octopus', 'has a thick monobrow'},),
+    "Squidward": ({'octopus', 'wears a brown t-shirt', 'is male'},),
+    "Squilliam": ({'octopus', 'has a thick monobrow', 'is male'},),
     "octopus": ({'doesn\'t have two legs', 'has a narrow face but a wide forehead and mouth'},),
 
     "starfish": ({'is coral-pink', 'has a cone-shaped head'},
                  {'is coral-pink', 'has red dots across its body'}),
-    "Patrick Star": ({'starfish', 'wears only a pair of shorts'},),
-    "Margie Star": ({'starfish', 'has hair'},),
+    "Patrick Star": ({'starfish', 'wears only a pair of shorts', 'is male'},),
+    "Margie Star": ({'starfish', 'has hair', 'is female'},),
     "Herb Star": ({'starfish', 'has a beard'},),
 
     "crustacean": ({'is red', 'has short, thin legs', 'has large pincers'},),
 
     "crab": ({'crustacean', 'has extremely tall eyes'},),
-    "Mr. Krabs": ({'crab', 'wears a suit'},),
+    "Mr. Krabs": ({'crab', 'wears a suit', 'is male'},),
     "Betsy Krabs": ({'crab', 'has wrinkles', 'is female'},),
     "Redbeard Krabs": ({'crab', 'has an eyepatch', 'has a beard'},
                        {'crab', 'wears a stripped sailor shirt'}),
 
     "lobster": ({'crustacean', 'has antennae'},),
-    "Larry the Lobster": ({'lobster'},),
+    "Larry the Lobster": ({'lobster', 'is male'},),
 
-    "plankton": ({'has only one eye', 'is green', 'has short, thin legs', 'has antennae'},),
-    "Sheldon J. Plankton": ({'plankton', 'is completely naked'},),
-    "Granny Plankton": ({'plankton', 'has wrinkles'},
-                        {'plankton', 'is female'},
-                        {'plankton', 'has hair'}),
+    "plankton": ({'has only one eye', 'is green', 'has short, thin legs',
+                  'has antennae'},),
+    "Sheldon J. Plankton": ({'plankton', 'is completely naked', 'is male'},),
+    "Granny Plankton": ({'plankton', 'is female', 'has hair', 'has wrinkles'},),
 
     "computer": ({"has a square head", "doesn't have two legs"},),
+    # gender is ambiguous, LOL
     "Karen Plankton": ({'computer', 'has a green line on the monitor'},),
     "Karen 2.0": ({'computer', 'has a red line on the monitor'},
-                  {'computer', 'has a slick body'})
+                  {'computer', 'has a slick body', 'is female'})
 }
 
 spongebob_exclusive_groups = ({'sponge', 'octopus', 'starfish', 'crustacean',
                                'plankton', 'computer'},
+                              {'is male', 'is female'},
                               {'is red', 'is green', 'is yellow', 'is coral-pink'})
 
 
