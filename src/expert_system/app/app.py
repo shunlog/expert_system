5863912f67ddd89e8943cd396ce2f7c1c117bd7c
shunlog <artiombalan331@gmail.com>
2024-10-01 00:11:45 +0300

    add the akinator page (stub)

--- src/expert_system/app/app.py
+++ src/expert_system/app/app.py
@@ -11,6 +11,16 @@ app = Flask(__name__)
 gt: GoalTree
 
 
+def rated_facts() -> list[tuple[str, dict]]:
+    facts = []
+    for node in gt.dag.all_terminals():
+        if node.truth is not None or node.pruned:
+            continue
+        facts.append((node.fact, node_value(gt, node)))
+    return sorted(facts, key=lambda v: v[1]
+                  ["value"], reverse=True)
+
+
 def reset_tree() -> None:
     global gt
     gt = GoalTree(spongebob_rules, spongebob_exclusive_groups)
@@ -29,7 +39,7 @@ def send_diagram_file():
 @app.post('/reset')
 def reset_tree_view():
     reset_tree()
-    return redirect('/')
+    return redirect(request.referrer or '/')
 
 
 @app.post('/answer')
@@ -37,25 +47,34 @@ def set_truth_view():
     truth = request.form['truth'] == 'true'
     fact = request.form['fact']
     set_fact_truth(fact, truth)
-    return redirect('/')
+    return redirect(request.referrer or '/')
+
+
+@app.route("/akinator")
+def akinator_view():
+    global gt
+    if sol := solution(gt.dag):
+        return render_template("akinator.html", solution=sol)
+    fact = rated_facts()[0][0]
+    return render_template("akinator.html", fact=fact)
+
+
+@app.route("/playground")
+def playground_view():
+    global gt
+    render_DAG(gt.dag, dir='/tmp/expert_system', fn='diagram', format="svg")
+    if sol := solution(gt.dag):
+        return render_template("playground.html", solution=sol)
+    return render_template("playground.html", fact_values=rated_facts())
 
 
 @app.route("/")
 def root_view():
-    global gt
-    render_DAG(gt.dag, dir='/tmp/expert_system', fn='diagram', format="svg")
-
-    if sol := solution(gt.dag):
-        return render_template("playground.html", solution=sol)
-    facts = []
-    for node in gt.dag.all_terminals():
-        if node.truth is not None or node.pruned:
-            continue
-        facts.append((node.fact, node_value(gt, node)))
-    facts = sorted(facts, key=lambda v: v[1]
-                   ["value"], reverse=True)
-
-    return render_template("playground.html", facts=facts)
+    return f'''
+    <li><a href={url_for("playground_view")}>Playground</a></li>
+    <li><a href={url_for("akinator_view")}>Akinator mode</a></li>
+    <li><a>Encyclopedia mode</a></li>
+    '''
 
 
 if __name__ == '__main__':
